445753e0997fa5b4e5ba5c7674da66b5aec6d55d
ytccoding <32517530+ytccoding@users.noreply.github.com>
2018-08-03 18:11:47 +0800

    配合01,增加延遲時間

---
+++ JS檢查_Web.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import subprocess
+from subprocess import PIPE
+from time import sleep
+
+startNumber = input("開始號碼(從1開始):").strip()
+endNumber = input("結束號碼:").strip()
+
+    
+for i in range(int(startNumber) ,int(endNumber)+1):
+   p = subprocess.Popen('JS檢查_chrome_1.3.1.exe' ,stdout=PIPE ,stderr=PIPE ,stdin=PIPE ,shell = True)
+   try:
+      p.communicate(input=str(i).encode("utf-8")  ,timeout = 2)
+   except:
+      pass
+   sleep(2)
+
+
